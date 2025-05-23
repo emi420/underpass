58e8f5c77260ba7e8c49045b4780b9ad79f425cd
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-05-23 18:19:37 -0400

    Support both std and boost filesystem lib

--- src/replicator/planetindex.cc
+++ src/replicator/planetindex.cc
@@ -29,10 +29,20 @@ using namespace boost::gregorian;
 #include <regex>
 #include <iomanip>
 #include <sstream>
-namespace fs = std::filesystem;
 #include "replicator/planetindex.hh"
 #include "utils/log.hh"
 using namespace logger;
+
+#if __has_include(<filesystem>)
+    #include <filesystem>
+    namespace fs = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#else
+    #include <boost/filesystem.hpp>
+    namespace fs = boost::filesystem;
+#endif
 
 namespace planetindex {
 
